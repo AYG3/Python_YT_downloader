f395f4a4980bdbe7666da9e2a816a033fdca2d21
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-03-14 18:45:58 +0100

    s

--- main2.py
+++ main2.py
@@ -2,6 +2,18 @@ import tkinter
 import customtkinter
 from pytube import YouTube
 
+
+def beginDownload():
+    try:
+        ytLink = link.get()
+        ytObject = YouTube(app)
+        vid = ytObject.streams.all
+        print(vid)
+    except:
+        print('Error')
+
+
+
 #System settings
 customtkinter.set_appearance_mode('dark')
 customtkinter.set_default_color_theme('green')
@@ -21,7 +33,7 @@ link = customtkinter.CTkEntry(app, textvariable=url_link, width=300)
 link.pack(padx=10, pady=10)
 
 #Download button
-button = customtkinter.CTkButton(app, text='Download')
+button = customtkinter.CTkButton(app, text='Download', command=beginDownload)
 button.pack()
 
 app.mainloop()
