cad4e52dc6ddbaa93039b3f390e2f4dd0c72f98c
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-03-14 18:41:48 +0100

    s

--- main2.py
+++ main2.py
@@ -11,8 +11,17 @@ app = customtkinter.CTk()
 app.geometry('720x480')
 app.title("YT Downloader - AYG3")
 
- 
+#Title
+title = customtkinter.CTkLabel(app, text='')
+title.pack()
+
 #Link
-link = customtkinter.CTkEntry(app, )
+url_link = tkinter.StringVar()
+link = customtkinter.CTkEntry(app, textvariable=url_link, width=300)
+link.pack(padx=10, pady=10)
+
+#Download button
+button = customtkinter.CTkButton(app, text='Download')
+button.pack()
 
 app.mainloop()
